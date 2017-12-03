92fbe81f5ec0e29d44cda48832dbdb7e67ad090b
Rafa Lima <paracafa@gmail.com>
2017-12-03 12:39:54 -0300

    Iniciado implementação carregar view pages com fragments trazidos em land e phone

--- app/src/main/java/com/example/orafa/visitabr/view/CountryActivity.java
+++ app/src/main/java/com/example/orafa/visitabr/view/CountryActivity.java
@@ -2,8 +2,14 @@ package com.example.orafa.visitabr.view;
 
 import android.content.Intent;
 import android.os.Parcelable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.orafa.visitabr.R;
 import com.example.orafa.visitabr.model.ClickStateListener;
@@ -11,12 +17,71 @@ import com.example.orafa.visitabr.model.Country;
 
 import org.parceler.Parcels;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CountryActivity extends AppCompatActivity implements ClickStateListener{
+
+    @BindView(R.id.view_pager)
+    ViewPager mViewPager;
+
+    @BindView(R.id.tab_layout)
+    TabLayout mTabLayout;
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_country);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        mViewPager.setAdapter(new CityPager(getSupportFragmentManager()));
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+
+    class CityPager extends FragmentPagerAdapter {
+
+        public CityPager(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new ListCountryFragment();
+            } else if (position == 1) {
+                return new ListCityWishFragment();
+            } else if (position == 2) {
+                return new ListCityWentFragment();
+            } else if (position == 3) {
+                return new ListCityWillFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) {
+                return getString(R.string.states);
+            } else if (position == 1) {
+                return getString(R.string.wish);
+            } else if (position == 2) {
+                return getString(R.string.went);
+            } else if (position == 3) {
+                return getString(R.string.will);
+            }
+            return null;
+        }
     }
 
     @Override
--- app/src/main/java/com/example/orafa/visitabr/view/ListCityActivity.java
+++ app/src/main/java/com/example/orafa/visitabr/view/ListCityActivity.java
@@ -1,32 +1,15 @@
 package com.example.orafa.visitabr.view;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 
 import com.example.orafa.visitabr.R;
 import com.example.orafa.visitabr.model.Country;
 
 import org.parceler.Parcels;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class ListCityActivity extends AppCompatActivity {
-
-    @BindView(R.id.view_pager)
-    ViewPager mViewPager;
-
-    @BindView(R.id.tab_layout)
-    TabLayout mTabLayout;
-
-    @BindView(R.id.toolbar)
-    Toolbar mToolbar;
 
     public static final String EXTRA_COUNTRY = "country";
 
@@ -34,52 +17,14 @@ public class ListCityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_city);
-        ButterKnife.bind(this);
 
-        setSupportActionBar(mToolbar);
+        Country country = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_COUNTRY));
 
-        mViewPager.setAdapter(new CityPager(getSupportFragmentManager()));
-        mTabLayout.setupWithViewPager(mViewPager);
-    }
+        ListCityFragment listCityFragment = ListCityFragment.newInstance(country);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.city, listCityFragment, "city")
+                .commit();
 
-    class CityPager extends FragmentPagerAdapter {
-
-        public CityPager(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            if (position == 0) {
-                Country country = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_COUNTRY));
-                return ListCityFragment.newInstance(country);
-            } else if (position == 1) {
-                return new ListCityWishFragment();
-            } else if (position == 2) {
-                return new ListCityWentFragment();
-            } else if (position == 3) {
-                return new ListCityWillFragment();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 4;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            if (position == 0) {
-                return getString(R.string.cities);
-            } else if (position == 1) {
-                return getString(R.string.wish);
-            } else if (position == 2) {
-                return getString(R.string.went);
-            } else if (position == 3) {
-                return getString(R.string.will);
-            }
-            return null;
-        }
     }
 }
