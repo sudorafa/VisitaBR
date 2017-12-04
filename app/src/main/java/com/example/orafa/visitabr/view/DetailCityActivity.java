f02f9ab50ce26ab93b97918cda1f6cd424f805c6
Rafa Lima <paracafa@gmail.com>
2017-12-04 09:53:25 -0300

    Aparencia de detalheCity concluido, próximo passo Persistencia para despois voltar para as telas de detalhes e edições de cidades.

--- app/src/main/java/com/example/orafa/visitabr/view/DetailCityActivity.java
+++ app/src/main/java/com/example/orafa/visitabr/view/DetailCityActivity.java
@@ -1,5 +1,8 @@
 package com.example.orafa.visitabr.view;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -11,14 +14,18 @@ import org.parceler.Parcels;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class DetailCityActivity extends AppCompatActivity {
 
     public static final String EXTRA_CITY = "city";
     CityPower mCityPower;
 
-    @BindView(R.id.text_view_test)
-    TextView textViewTest;
+    @BindView(R.id.text_view_name)
+    TextView textViewName;
+
+    @BindView(R.id.float_button_wish)
+    FloatingActionButton floatButtonWish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,38 @@ public class DetailCityActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
-        textViewTest.setText(mCityPower.getName());
+        textViewName.setText(mCityPower.getName());
+        toggleWish();
+
     }
+
+    private void toggleWish(){
+        boolean wish = isFav("rafaa");
+        floatButtonWish.setBackgroundTintList(
+                wish ? ColorStateList.valueOf(Color.RED) : ColorStateList.valueOf(Color.GREEN)
+        );
+    }
+
+    @OnClick(R.id.float_button_wish)
+    public void wishClick(){
+        String rafa = "rafa";
+        if(isFav(rafa)){
+            //remove
+        }else{
+            //add
+        }
+    }
+
+    //fazer +/- isso no DAO
+    public boolean isFav(String rafa){
+        String rafaAqui = "rafa";
+        boolean yes = false;
+
+        if(rafaAqui.equals(rafa)){
+            yes = true;
+        }
+
+        return yes;
+    }
+
 }
